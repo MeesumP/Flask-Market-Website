7a87b055563f7dfb12214021c7f3dda0590fc228
Meesum Panjwani <meesumusa@hotmail.com>
2021-08-14 13:53:44 -0400

    added market HTML5 page

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+@app.route('/home')
 def home_page():
     return render_template('home.html')
+
+@app.route('/market')
+def market():
+    return render_template('market.html')
